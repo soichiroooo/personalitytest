806e468f72fbbbf9de9cc57dfb82207342168e44
soichiroooo <slamdunkducksky@gmail.com>
2022-12-06 18:29:09 +0900

    testsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,62 @@
 ActiveRecord::Schema.define(version: 2022_12_05_025520) do
 
   create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "item1", null: false
+    t.integer "item2", null: false
+    t.integer "item3", null: false
+    t.integer "item4", null: false
+    t.integer "item5", null: false
+    t.integer "item6", null: false
+    t.integer "item7", null: false
+    t.integer "item8", null: false
+    t.integer "item9", null: false
+    t.integer "item10", null: false
+    t.integer "item11", null: false
+    t.integer "item12", null: false
+    t.integer "item13", null: false
+    t.integer "item14", null: false
+    t.integer "item15", null: false
+    t.integer "item16", null: false
+    t.integer "item17", null: false
+    t.integer "item18", null: false
+    t.integer "item19", null: false
+    t.integer "item20", null: false
+    t.integer "item21", null: false
+    t.integer "item22", null: false
+    t.integer "item23", null: false
+    t.integer "item24", null: false
+    t.integer "item25", null: false
+    t.integer "item26", null: false
+    t.integer "item27", null: false
+    t.integer "item28", null: false
+    t.integer "item29", null: false
+    t.integer "item30", null: false
+    t.integer "item31", null: false
+    t.integer "item32", null: false
+    t.integer "item33", null: false
+    t.integer "item34", null: false
+    t.integer "item35", null: false
+    t.integer "item36", null: false
+    t.integer "item37", null: false
+    t.integer "item38", null: false
+    t.integer "item39", null: false
+    t.integer "item40", null: false
+    t.integer "item41", null: false
+    t.integer "item42", null: false
+    t.integer "item43", null: false
+    t.integer "item44", null: false
+    t.integer "item45", null: false
+    t.integer "item46", null: false
+    t.integer "item47", null: false
+    t.integer "item48", null: false
+    t.integer "item49", null: false
+    t.integer "item50", null: false
+    t.integer "item51", null: false
+    t.integer "item52", null: false
+    t.integer "item53", null: false
+    t.integer "item54", null: false
+    t.integer "item55", null: false
+    t.integer "item56", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
