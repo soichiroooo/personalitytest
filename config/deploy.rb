3dc6dc7469b55e9a12f72d98fc5af78c28a8a407
soichiroooo <slamdunkducksky@gmail.com>
2022-12-06 15:36:06 +0900

    デプロイエラー解消方法模索

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,11 +14,8 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, { 
-  auth_methods: %w(publickey),
-  forward_agent: false,
-  keys: %w(~/.ssh/falcon.pem)
-}
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/falcon.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
