e9881738d1ec13dd8ebfc166076e58ae2a00f550
soichiroooo <slamdunkducksky@gmail.com>
2022-12-11 17:13:21 +0900

    テスト前半部の重複を禁止

--- app/javascript/answer.js
+++ app/javascript/answer.js
@@ -1,4 +1,31 @@
 window.addEventListener('load', function(){
+// 前半のテストで重複を許さないための記述
+  const Buttons = document.querySelectorAll(".button");
+
+  Buttons.forEach(function(Button){
+    Button.addEventListener('click',function(){
+      switch(this.id%4){
+        case null||1:
+          document.getElementById(`test_item${Number(this.id)+1}_${ this.dataset.val }`).checked = false;
+          document.getElementById(`test_item${Number(this.id)+2}_${ this.dataset.val }`).checked = false;
+          document.getElementById(`test_item${Number(this.id)+3}_${ this.dataset.val }`).checked = false;
+        case 2:
+          document.getElementById(`test_item${Number(this.id)-1}_${ this.dataset.val }`).checked = false;
+          document.getElementById(`test_item${Number(this.id)+1}_${ this.dataset.val }`).checked = false;
+          document.getElementById(`test_item${Number(this.id)+2}_${ this.dataset.val }`).checked = false;
+        case 3:
+          document.getElementById(`test_item${Number(this.id)-2}_${ this.dataset.val }`).checked = false;
+          document.getElementById(`test_item${Number(this.id)-1}_${ this.dataset.val }`).checked = false;
+          document.getElementById(`test_item${Number(this.id)+1}_${ this.dataset.val }`).checked = false;
+        case 0:
+          document.getElementById(`test_item${Number(this.id)-3}_${ this.dataset.val }`).checked = false;
+          document.getElementById(`test_item${Number(this.id)-2}_${ this.dataset.val }`).checked = false;
+          document.getElementById(`test_item${Number(this.id)-1}_${ this.dataset.val }`).checked = false;
+      };
+    });
+  });
+
+// 後半のテストで重複を許さないための記述
   const aButtons = document.querySelectorAll(".a_button");
   const bButtons = document.querySelectorAll(".b_button");
 
@@ -6,16 +33,7 @@ window.addEventListener('load', function(){
     aButton.addEventListener('click', function(){
       const aValue = this.dataset.val;
       const bButton = document.getElementById(`test_item${Number(this.id)+1}_${4-aValue }`);
-        bButton.checked = true
+      bButton.checked = true
     });
-  });
-
-  const Buttons = document.querySelectorAll(".button");
-  // const secondButtons = document.querySelectorAll(".second_button");
-  // const thirdButtons = document.querySelectorAll(".third_button");
-  // const fourthButtons = document.querySelectorAll(".fourth_button");
-
-  Buttons.forEach(function(Button){
-    const Button = document.getElementById(`test_item${Number(this.id)+1}_${4-aValue }`);
   });
 });
