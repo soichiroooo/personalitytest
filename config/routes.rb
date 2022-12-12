5841bd584b8944381441bcb594a2817b8a392f63
soichiroooo <slamdunkducksky@gmail.com>
2022-12-12 18:03:57 +0900

    性格診断結果表示のアクションをshowに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   root to:"tests#new"
-  resources :tests, only: [:new, :create] do
-    collection do
-      get 'result'
-    end
-  end
+  resources :tests, only: [:new, :create, :show]
 end
