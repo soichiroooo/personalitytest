180df92416b3fdd3958fc4d246cc759e8ace1009
soichiroooo <slamdunkducksky@gmail.com>
2022-12-12 10:14:16 +0900

    テスト機能実装

--- app/javascript/answer.js
+++ app/javascript/answer.js
@@ -5,22 +5,26 @@ window.addEventListener('load', function(){
   Buttons.forEach(function(Button){
     Button.addEventListener('click',function(){
       switch(this.id%4){
-        case null||1:
+        case 1:
           document.getElementById(`test_item${Number(this.id)+1}_${ this.dataset.val }`).checked = false;
           document.getElementById(`test_item${Number(this.id)+2}_${ this.dataset.val }`).checked = false;
           document.getElementById(`test_item${Number(this.id)+3}_${ this.dataset.val }`).checked = false;
+        break;
         case 2:
           document.getElementById(`test_item${Number(this.id)-1}_${ this.dataset.val }`).checked = false;
           document.getElementById(`test_item${Number(this.id)+1}_${ this.dataset.val }`).checked = false;
           document.getElementById(`test_item${Number(this.id)+2}_${ this.dataset.val }`).checked = false;
+        break;
         case 3:
           document.getElementById(`test_item${Number(this.id)-2}_${ this.dataset.val }`).checked = false;
           document.getElementById(`test_item${Number(this.id)-1}_${ this.dataset.val }`).checked = false;
           document.getElementById(`test_item${Number(this.id)+1}_${ this.dataset.val }`).checked = false;
+        break;
         case 0:
           document.getElementById(`test_item${Number(this.id)-3}_${ this.dataset.val }`).checked = false;
           document.getElementById(`test_item${Number(this.id)-2}_${ this.dataset.val }`).checked = false;
           document.getElementById(`test_item${Number(this.id)-1}_${ this.dataset.val }`).checked = false;
+        break;
       };
     });
   });
@@ -36,4 +40,16 @@ window.addEventListener('load', function(){
       bButton.checked = true
     });
   });
+
+  // const formObj = document.getElementById("test-form")
+  // formObj.addEventListener('change', function(){
+  //   const input33 = document.querySelectorAll('input[name="test[item33]"]')
+  //   const input34 = document.querySelectorAll('input[name="test[item34]"]')
+  //   const input33Item1 = document.getElementById("test_item33_4")
+  //   console.log("ok")
+  //   console.log(input33)
+
+  //   console.log(input33Item1.checked)
+  //   console.log(input34[0].value)
+  // });
 });
