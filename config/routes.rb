63b62abd326776fd8dd63b7177b92868d06dc599
soichiroooo <slamdunkducksky@gmail.com>
2022-12-12 10:43:23 +0900

    テスト結果確認ページの設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to:"tests#new"
-  resources :tests, only: [:new, :create]
+  resources :tests, only: [:new, :create] do
+    collection do
+      get 'result'
+    end
+  end
 end
