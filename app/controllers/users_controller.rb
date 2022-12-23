619bc10a99901b30cc9987d780cd131dfa86ae66
soichiroooo <slamdunkducksky@gmail.com>
2022-12-23 19:29:15 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def show
-    @test = Test.find(User.find(current_user.id).test_id) || nil
+    @test = Test.find(User.find(params[:id]).test_id) || nil
+    @user = User.find(params[:id])
   end
 end
