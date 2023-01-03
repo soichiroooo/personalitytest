08340f31668de1d4987f208dc22d8a01762056f3
soichiroooo <slamdunkducksky@gmail.com>
2023-01-03 11:30:37 +0900

    会員登録を先に行なった場合のマイページ表示の条件分岐を設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def show
-    @test = Test.find(User.find(params[:id]).test_id) || nil
+    @test = Test.find_by(id: User.find(params[:id]).test_id)
     @user = User.find(params[:id])
   end
 end
