f45bc2a585b39855ff5dc51f7a4503ccb2ef01fa
soichiroooo <slamdunkducksky@gmail.com>
2023-02-27 19:14:37 +0900

    ユーザー登録のバリデーションを規定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)(?=.*?[!-\/:-@\[-`{-~])[!-~]+\z/i }
+  validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)(?=.*?[!-\/:-@\[-`{-~])[!-~]+\z/i }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format:  { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :birthday, presence: true
+
 end
